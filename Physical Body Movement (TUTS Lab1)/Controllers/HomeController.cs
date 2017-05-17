a90e46a710bae4e4786e0a91653884573928206b
Max Bondarchuk <bondarchuk.m.y@gmail.com>
2017-05-17 12:49:35 +0300

    Lab3 InvariantCulture

--- Physical Body Movement (TUTS Lab1)/Controllers/HomeController.cs
+++ Physical Body Movement (TUTS Lab1)/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Physical_Body_Movement__TUTS_Lab1_.Models;
+using System.Globalization;
 
 namespace Physical_Body_Movement__TUTS_Lab1_.Controllers
 {
@@ -32,11 +33,11 @@ namespace Physical_Body_Movement__TUTS_Lab1_.Controllers
         [HttpGet]
         public ChartData GetChartData(string tu, string tg, string t, string thetaCoefficient, string n, int type)
         {
-            if (!double.TryParse(tu, out double tuD) ||
-                !double.TryParse(tg, out double tgD) ||
-                !double.TryParse(t, out double tD) ||
-                !double.TryParse(thetaCoefficient, out double thetaCoefficientD) ||
-                !double.TryParse(n, out double nD))
+            if (!double.TryParse(tu, NumberStyles.Any, CultureInfo.InvariantCulture, out double tuD) ||
+                !double.TryParse(tg, NumberStyles.Any, CultureInfo.InvariantCulture, out double tgD) ||
+                !double.TryParse(t, NumberStyles.Any, CultureInfo.InvariantCulture, out double tD) ||
+                !double.TryParse(thetaCoefficient, NumberStyles.Any, CultureInfo.InvariantCulture, out double thetaCoefficientD) ||
+                !double.TryParse(n, NumberStyles.Any, CultureInfo.InvariantCulture, out double nD))
                 return new ChartData();
 
             return Physics.Calculate(tuD, tgD, tD, thetaCoefficientD, nD, type);
